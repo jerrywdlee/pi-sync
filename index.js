a8e4d09848080a9c2091f1ae5947fe53418a4fc1
Jerry Lee <lzjjerrywoodylee@gmai.com>
2017-12-29 02:50:39 +0900

    1. update commands 2. update message shown on events 3. update watch logic

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const YAML = require('yamljs');
 const colors = require('colors/safe');
 const _ = require('lodash');
 
+const { connect_opt } = require('./lib/argv_helper');
 const walkSync = require('./lib/ignore_walk');
 const watch = require('./lib/watch_dir');
 const Sync = require('./lib/sync_files');
@@ -22,29 +23,49 @@ try {
   let warn = `Could not find ${colors.green(`'${confFileName}'`)} under ${colors.yellow(process.cwd())}`;
   console.warn(colors.red(`[Warning]`), warn);
   throw Error(e.message)
+} finally {
+  if (syncConf.connection) {
+    Object.assign(syncConf.connection, connect_opt);
+  } else {
+    syncConf.connection = connect_opt;
+  }
 }
+
+
 
 let ignoreRules = ['.git'].concat(syncConf.ignore).filter(r => r);
 let includeRules = [].concat(syncConf.include).filter(r => r).map(r => r.trim());
 
 const { fileList, ignoreRuleList } = walkSync({ ignoreFiles: [] }, includeRules, ignoreRules);
 
-// global variables
-let putFileList = [];
-let delFileList = [];
-let busyFlag = false;
-
 console.log(syncConf);
 console.log(fileList);
-
-watch(ignoreRuleList);
 
 const sync = new Sync(syncConf);
 (async () => {
   try {
     await sync.connect();
-    await sync.sync(fileList, fileList);
+    const connectMsg = `Connected to "${colors.bold.yellow(
+      syncConf.connection.username + '@' +
+      syncConf.connection.host + ':' + syncConf.connection.port
+    )}" !`;
+    console.log(
+      colors.bold.green(`[pi-sync]`),
+      colors.green(connectMsg),
+    );
+    await sync.sync(fileList);
+    if (syncConf.connection.watch) {
+      const opt = {
+        ignoreRuleList,
+        sync,
+        syncConf
+      }
+      watch(opt);
+    } else {
+      sync.close();
+    }
   } catch (error) {
-    console.log(error);
+    console.error('Err!', error.message);
+    process.exit(1);
   }
-})()
+})();
